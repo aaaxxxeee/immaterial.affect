5b1ed2cde19966240e3f2abed56797f2f51e5868
alex beuzen <90225612+aaaxxxeee@users.noreply.github.com>
2025-10-21 15:27:57 -0400

    Can now run the decomposition on every .png file of the hdd folder

--- immaterialaffect.py
+++ immaterialaffect.py
@@ -1,24 +1,16 @@
 # lib
 from PIL import Image
-import random
-import os
+import random, os
 
-def decompose(img_path, portiontodelete=1, output_path=None):
+def decompose(p,portion=1,o=None):
+    i=Image.open(p).convert('RGBA');px=i.load();w,h=i.size
+    for y in range(h):
+        for x in range(w):
+            if random.random()<portion:
+                r,g,b,a=px[x,y];px[x,y]=(r,g,b,0)
+    i.save(o or p)
 
-# ouvrir image
-    img = Image.open(img_path).convert("RGBA")
-    pixels = img.load()
-    width, height = img.size
-
-    for y in range(height):
-        for x in range(width):
-            if random.random() < portiontodelete:
-                r, g, b, a = pixels[x, y]
-                pixels[x, y] = (r, g, b, 0)  # transparent pixel
-
-# save
-    if not output_path:
-        output_path = image_path 
-    img.save(output_path)
-
-decompose("hdd/igprofile.png", portiontodelete=1, output_path="hdd/igprofile.png")
+for r,_,fs in os.walk('hdd'):
+    for f in fs:
+        if f.lower().endswith(('.png','.jpg','.jpeg')):
+            decompose(os.path.join(r,f),portion=0.05)
