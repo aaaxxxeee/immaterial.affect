908751c0ec08225798d43ebcbb1c8af643b97e0a
alex beuzen <90225612+aaaxxxeee@users.noreply.github.com>
2025-11-19 23:33:48 -0500

    added auto conversion of jpg / jpeg to png

--- immaterialaffect.py
+++ immaterialaffect.py
@@ -2,6 +2,17 @@
 from PIL import Image
 import random, os
 
+def convert_jpgs_to_pngs(folder):
+    for r, _, fs in os.walk(folder):
+        for f in fs:
+            if f.lower().endswith(('.jpg', '.jpeg')):
+                p = os.path.join(r, f)
+                im = Image.open(p).convert('RGBA')
+                out = os.path.splitext(p)[0] + '.png'
+                im.save(out, 'PNG')
+                im.close()
+                os.remove(p)
+
 def decompose(p,portion=1,o=None):
     i = Image.open(p)
     px = i.load(); w, h = i.size
@@ -15,9 +26,11 @@ def decompose(p,portion=1,o=None):
                     r, g, b = val; px[x, y] = (r, g, b)
     i.save(o or p)
 
-while True:
-    for r,_,fs in os.walk('hdd'):
-        for f in fs:
-            if f.lower().endswith(('.png','.jpg','.jpeg')):
-                decompose(os.path.join(r,f),portion=0.05)
-                print("memory deterioration in progress on " + str(f))
+if __name__ == '__main__':
+    convert_jpgs_to_pngs('hdd')
+    while True:
+        for r, _, fs in os.walk('hdd'):
+            for f in fs:
+                if f.lower().endswith('.png'):
+                    decompose(os.path.join(r, f), portion=0.05)
+                    print("memory deterioration in progress on " + str(f))
